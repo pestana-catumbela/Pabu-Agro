015526fb24cf6079c900b1eb1dfda537cbe83960
Alan Ferreira <ti026@pacaembuautopecas.com.br>
2023-10-05 16:54:24 -0300

    Finalização do header

--- src/components/HeaderComponents/LogoHeaderComponents.vue
+++ src/components/HeaderComponents/LogoHeaderComponents.vue
@@ -1,21 +1,44 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+import LogoPabuAgro from "../../assets/img/logo.png"
+
+const pabuAgroInfo = reactive({
+  logo: LogoPabuAgro,
+  name: "PACAEMBU"
+})
+</script>
 
 <template>
   <div class="logo">
     <div class="logo__box">
-      <span class="logo__box__element">Pabu Agro</span>
+      <img class="logo__box__img" :src="pabuAgroInfo.logo" alt="">
+      <span class="logo__box__name" v-html="pabuAgroInfo.name"></span>
     </div>
   </div>
 </template>
 
 <style scoped>
 .logo {
-  background-color: aqua;
+  background-color: #004A35;
   display: flex;
-  justify-content: center;
-  height: 60px;
-}
+  justify-content: center; 
+  align-items: center;
+  height: 80px;
+  color: #fff;
+} 
 .logo__box {
   width: 80%;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.logo__box__img{
+  width: 30px;
+}
+
+.logo__box__name{
+  font-weight: 700;
+  font-size: 20px;
 }
 </style>
